77272ad05d8824b2a1a9286c57d97652359cf7ea
Stuart Rawlinson <s.rawlinson89@gmail.com>
2019-02-06 15:43:29 +0000

    Finished Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 @login_required
 def add_category(request):
@@ -62,6 +63,9 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context_dict)
 
 def index(request):
+    #Line added first to test cookies. 
+    request.session.set_test_cookie()
+
     #Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     # context_dict = { 'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
@@ -78,16 +82,32 @@ def index(request):
 
     context_dict = {'categories': category_list, 'pages': pages_list} 
 
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier. 
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context_dict)
+    # Obtain our Response object early so we can add cookie information
+    response = render(request, 'rango/index.html', context_dict)
+
+
+    # Return response back to the user, updating any cookies that need changed. 
+    return response
     
 
 def about(request):
+    # Lines added for cookie testing (Chapter 10)
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
+    
     context_dict = { 'boldmessage': "A less weird message."}
-    return render(request, 'rango/about.html', context=context_dict)
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/about.html', context=context_dict) 
+    return response
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass 
@@ -224,7 +244,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you are logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
@@ -232,5 +252,37 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage. 
     return HttpResponseRedirect(reverse('index'))
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Helper function for tracking cookies
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie. 
+    # If the cookie exists, the value returned is casted to an integer. 
+    # If the cookie doesn't exist, then the default value of 1 is used. 
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    
+    #If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last cookie visit now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
 
 
